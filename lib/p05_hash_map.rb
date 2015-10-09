3a66ec9664c274c2641b9035a4299dca57c3fc14
App Academy Student <appacademy@Apps-Mac-mini-30.local>
2015-10-09 16:19:52 -0400

    functioning hash map.

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -2,6 +2,8 @@ require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
+
   attr_reader :count
 
   def initialize(num_buckets = 8)
@@ -14,17 +16,29 @@ class HashMap
   end
 
   def set(key, val)
+    if @count == num_buckets
+      resize!
+    end
     @store[bucket(key)].insert(key, val)
     @count += 1
   end
 
   def get(key)
+    @store[bucket(key)].get(key)
   end
 
   def delete(key)
+    @store[bucket(key)].remove(key)
+    @count -= 1
   end
 
   def each
+    @store.each do |bucket|
+      bucket.each do |el|
+        yield el.key, el.val
+      end
+    end
+    return self
   end
 
   # uncomment when you have Enumerable included
@@ -45,6 +59,14 @@ class HashMap
   end
 
   def resize!
+    old_store = self.map{|k,v| [k,v] }
+    @store = Array.new(num_buckets * 2) { LinkedList.new }
+    @count = 0
+    old_store.each do |pair|
+      k,v = pair
+      set(k,v)
+    end
+
   end
 
   def bucket(key)
